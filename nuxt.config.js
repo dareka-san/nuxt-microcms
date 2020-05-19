6850278005e36c8dd019c313e566c58e8cb9e0cd
dareka-san <kemuri103@gmail.com>
2020-05-19 16:36:06 +0900

    create: first build

---
+++ nuxt.config.js
@@ -0,0 +1,95 @@
+require("dotenv").config();
+const { API_KEY } = process.env;
+const axios = require("axios");
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/markdownit'
+  ],
+  markdownit: {
+    html: true,
+    injected: true,
+    preset: "default"
+  },
+  /*
+  ** dot env
+  */
+  env: {
+    API_KEY
+  },
+  /*
+  ** generate
+  */
+  generate: {
+    routes() {
+      const careers = axios
+        .get("https://salisty.microcms.io/api/v1/careers", {
+          headers: { "X-API-KEY": process.env.API_KEY }
+        })
+        .then(res => {
+          return res.data.contents.map(career => {
+            return "/careers/" + career.id;
+          });
+        });
+      const posts = axios
+        .get("https://salisty.microcms.io/api/v1/posts", {
+          headers: { "X-API-KEY": process.env.API_KEY }
+        })
+        .then(res => {
+          return res.data.contents.map(post => {
+            return "/careers/posts/" + post.id;
+          });
+        });
+      return Promise.all([careers, posts]).then(values => {
+        return values.join().split(",");
+      });
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-for="item in items" :key="item.id">
+      <nuxt-link :to="'careers/' + item.id">
+        <h2>
+          {{ item.name }}
+          {{ item.id }}
+        </h2>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  async asyncData() {
+    const { data } = await axios.get(
+      "https://salisty.microcms.io/api/v1/careers",
+      {
+        headers: { "X-API-KEY": process.env.API_KEY }
+      }
+    );
+    return {
+      items: data.contents
+    };
+  }
+};
+</script>
